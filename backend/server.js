490fe0353bdb6a37367ee27d1a670ac734972df9
Rishabh Singh <itsindrajput@gmail.com>
2025-05-16 20:29:01 +0530

    Backend Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,49 +1,37 @@
-import express  from "express"
-import cors from 'cors'
-import { connectDB } from "./config/db.js"
-import foodRouter from "./routes/foodRoute.js"
-import userRouter from "./routes/userRoute.js"
-import 'dotenv/config'
-import cartRouter from "./routes/cartRoute.js"
-import orderRouter from "./routes/orderRoute.js"
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import "dotenv/config";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
+// app config
+const app = express();
+const port = 4000;
 
+// middlewares
+app.use(express.json());
+app.use(cors());
 
+// db connection
+connectDB();
 
- // app config
- const app = express()
- const port =  4000;
+// api endpoints
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
 
- // middlewares
- app.use(express.json())
- app.use(cors())
-
-
- // db connection
- connectDB();
-
-
- 
- // api endpoints
- app.use("/api/food", foodRouter);
-app.use("/images", express.static('uploads'));
-app.use("/api/user",userRouter)
-app.use("/api/cart",cartRouter)
-app.use("/api/order",orderRouter)
-
- app.get("/", (req, res) => {
-         res.send("API Working")
-       })
-    
-app.listen(port,()=>{
-  console.log(`Server started on http://localhost:${port}`)
-})
-    
-
-
-
-
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
 
 // import express  from "express"
 // import cors from 'cors'
@@ -57,7 +45,6 @@ app.listen(port,()=>{
 // // app config
 // const app = express()
 // const port = process.env.PORT || 4000;
-
 
 // // middlewares
 // app.use(express.json())
