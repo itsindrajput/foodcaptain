02d67a630492ecad27931928729ac2ce35fab861
Rishabh Singh <itsindrajput@gmail.com>
2025-05-16 20:42:08 +0530

    Backend Deployment

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import orderRouter from "./routes/orderRoute.js";
 
 // app config
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 // middlewares
 app.use(express.json());
@@ -30,38 +30,6 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server started on http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
 
-// import express  from "express"
-// import cors from 'cors'
-// import { connectDB } from "./config/db.js"
-// import userRouter from "./routes/userRoute.js"
-// import foodRouter from "./routes/foodRoute.js"
-// import 'dotenv/config'
-// import cartRouter from "./routes/cartRoute.js"
-// import orderRouter from "./routes/orderRoute.js"
-
-// // app config
-// const app = express()
-// const port = process.env.PORT || 4000;
-
-// // middlewares
-// app.use(express.json())
-// app.use(cors())
-
-// // db connection
-// connectDB()
-
-// // api endpoints
-// app.use("/api/user", userRouter)
-// app.use("/api/food", foodRouter)
-// app.use("/images",express.static('uploads'))
-// app.use("/api/cart", cartRouter)
-// app.use("/api/order",orderRouter)
-
-// app.get("/", (req, res) => {
-//     res.send("API Working")
-//   });
-
-// app.listen(port, () => console.log(`Server started on http://localhost:${port}`))
