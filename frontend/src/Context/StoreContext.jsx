a4945f048d497da8f947376146e1ae07f9db235f
Rishabh Singh <itsindrajput@gmail.com>
2025-05-26 12:02:26 +0530

    Project Deployment

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "https://foodcaptain-backend.vercel.app/";
+  const url = "https://foodcaptain.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
